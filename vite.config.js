7b09019199afed4de34f2f6bcb8e6ab886c025d3
Joel <joeljiyane@gmail.com>
2026-08-04 20:15:56 -0500

    chore: optimizar configuración base y routing de .htaccess para Hostinger

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Garantiza compatibilidad universal en Hostinger, cPanel o cualquier hosting estático
 })
